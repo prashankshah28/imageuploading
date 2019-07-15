8e1a83e5d9670eb98abc7ddf5f2f671e4d1bdabc
prashankshah28 <you@example.com>
2019-07-15 19:05:32 +0530

    Reset Button Working

--- listing.php
+++ listing.php
@@ -1,64 +1,67 @@
 <html>
-   <head>
-      <title>
-      </title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-      
-      <!-- jQuery library -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<head>
+  <title>
+  </title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 
-      <!-- jQuery library -->
-      
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-      <!-- Popper JS -->
-      
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-      <!-- Latest compiled JavaScript -->
-      
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-      
-        <!-- jQuery UI library -->
-    
+  <!-- jQuery library -->
 
-      <!-- jQuery library -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Popper JS -->
 
-      <!-- jQuery UI library -->
-      <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <!-- Latest compiled JavaScript -->
 
-      <!-- js notify  -->
-      <script type="text/javascript" src="js/notify.min.js"></script>
-      <script type="text/javascript" src="js/notify.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-   </head>
-   <body>
+  <!-- jQuery UI library -->
 
-<br>
-      <div class="col-md-12">
-          <h2 class="text-center">All Uploaded Images</h2>
-      </div>
-      <div class="container">
-        <div class="col-md-6 float-right">
-        <a href="index.php"  class="btn btn-primary float-right">Click here For add Image </a>
-      </div>
-      <div class="col-md-6">
-        <input type="text" name="search" id="search"/>
-        <input type="submit" value="Search" class="btn-primary" onclick='search_name()' />
+
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+  <!-- jQuery UI library -->
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+  <!-- js notify  -->
+  <script type="text/javascript" src="js/notify.min.js"></script>
+  <script type="text/javascript" src="js/notify.js"></script>
+
+</head>
+<body>
+
+  <br>
+  <div class="col-md-12">
+    <h2 class="text-center">All Uploaded Images</h2>
+  </div>
+  <div class="container">
+    <div class="col-md-6 float-right">
+      <a href="index.php"  class="btn btn-primary float-right">Click here For add Image </a>
     </div>
+    <form>
+    <div class="col-md-6">
+      <input type="text" name="search" id="search"/>
+      <input type="submit" value="Search" class="btn-primary" onclick='search_name()' />
+      <input type="reset" value="Reset" />
+    </div>
+  </form>
 
-      </div>
+  </div>
 
-<br>
+  <br>
 
-      <div class="container">
-         <div class="table-responsive">
-            <table class="table table-striped table-bordered" id="imageData">
-            </table>
-         </div>
-      </div>
-   </body>
+  <div class="container">
+   <div class="table-responsive">
+    <table class="table table-striped table-bordered" id="imageData">
+    </table>
+  </div>
+</div>
+</body>
 </html>
 <script>
 
@@ -66,38 +69,38 @@
 function delete_img(id)
 {
 //    alert("hey");
-    var image_id = id;
+var image_id = id;
     //alert(image_id);
     if(confirm("Are you sure you want to delete this?")){
-    delete_image(image_id);
+      delete_image(image_id);
     }
-}
+  }
 //call by delete_img
 function delete_image(image_id)
 {
 
-    $.ajax({
+  $.ajax({
     url: 'delete.php',
     type: 'POST',
     data:{
-    delete_image:'delete_image',
-    image_id:image_id,
-  },
-  success:function(response) {
-   if(response=="Delete Successfully")
-   {
+      delete_image:'delete_image',
+      image_id:image_id,
+    },
+    success:function(response) {
+     if(response=="Delete Successfully")
+     {
 //        $.notify("Hello World");
-        
-        load_image_list();
-        $.notify("Delete Successfully", "success");
+
+load_image_list();
+$.notify("Delete Successfully", "success");
         //window.location = "listing.php";
 //    document.location.reload();   
-   }
-  }
- });
 }
- function load_image_list()
- {
+}
+});
+}
+function load_image_list()
+{
   var action = "fetch";
   $.ajax({
    url:"displayImages.php",
@@ -106,9 +109,9 @@ function delete_image(image_id)
    success:function(data)
    {
     $('#imageData').html(data);
-   }
-  });
- };
+  }
+});
+};
 
 </script>
 <script>
@@ -120,23 +123,23 @@ function delete_image(image_id)
       url:"search.php",
       method:"POST",
       data:{action:action,search_name:search_name},
-    success:function(data)
-   {
+      success:function(data)
+      {
 //    alert(data);
-    $('#imageData').html(data);
-   }
-  });
- };
-load_image_list();
+$('#imageData').html(data);
+}
+});
+  };
+  load_image_list();
 
 </script>
 
 
 <script>
-$(function() {
+  $(function() {
     $("#search").autocomplete({
-        source: "searchimage.php",
+      source: "searchimage.php",
     });
-});
+  });
 
 </script>
